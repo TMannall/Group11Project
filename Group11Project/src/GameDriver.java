6ef86fb940675398b7c15146f08b3a490e39d70b
Rohjo <rohjo@hotmail.co.uk>
2016-01-26 12:21:37 +0000

    Menu placeholder texture placed

--- Group11Project/src/GameDriver.java
+++ Group11Project/src/GameDriver.java
@@ -7,6 +7,14 @@ import org.jsfml.window.*;
 import org.jsfml.window.event.*;
 
 public class GameDriver {
+    public static int getWinWidth() {
+        return WIN_WIDTH;
+    }
+
+    public static int getWinHeight() {
+        return WIN_HEIGHT;
+    }
+    private GameDriver driver = this;
     private static final int WIN_WIDTH = 1280;
     private static final int WIN_HEIGHT = 720;
     private static final String TITLE = "Endless Sea";
@@ -21,8 +29,9 @@ public class GameDriver {
         window.create(new VideoMode(WIN_WIDTH, WIN_HEIGHT), TITLE, WindowStyle.DEFAULT);
         window.setFramerateLimit(30);
 
+        Textures textures = new Textures();
         machine = new FSM();
-        menu = new Menu(window);
+        menu = new Menu(driver, window, textures);
         game = new Game(window);
 
         // Set menu state for game launch
