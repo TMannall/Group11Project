310df7fa05bf0877f3a469613589b449d4492691
Jack <jackkeates@gmail.com>
2016-01-26 11:49:50 +0000

    updates to textures
    cleaning

--- Group11Project/src/Textures.java
+++ Group11Project/src/Textures.java
@@ -7,44 +7,28 @@ import java.nio.file.Paths;
 
 public class Textures {
 
-    public LoadTexture mainMenu;
-    public LoadTexture captainTestPortrait;
+    public Sprite mainMenu;
+    public Sprite captainTestPortrait;
 
     public Textures() {
-        mainMenu = new LoadTexture("textures/main_menu.png");
-        captainTestPortrait = new LoadTexture("textures/captain_portrait_example.png");
+        mainMenu = LoadTexture("textures/main_menu.png");
+        captainTestPortrait = LoadTexture("textures/captain_portrait_example.png");
     }
 
 
-    public class LoadTexture {
-        public LoadTexture(String fileName) {
-            Texture texture = new Texture();
-            try {
-                texture.loadFromFile(Paths.get(fileName));
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-            texture.setSmooth(true);
-        }
-    }
+    public Sprite LoadTexture(String fileName) {
+        Texture texture = new Texture( );
 
-    /**
-     * takes the
-     */
-    public class LoadSprite {
-        public LoadSprite(String fileName) {
-            Texture texture = new Texture( );
-            try {
-                texture.loadFromFile(Paths.get(fileName));
-            } catch (IOException ex) {
-                ex.printStackTrace( );
-            }
-            texture.setSmooth(true);
-
-            Sprite img = new Sprite(texture);
-            img.setOrigin(Vector2f.div(
-                    new Vector2f(texture.getSize()), 2));
+        try {
+            texture.loadFromFile(Paths.get(fileName));
+        } catch (IOException ex) {
+            ex.printStackTrace( );
         }
+
+        texture.setSmooth(true);
+
+        Sprite image = new Sprite(texture);
+        return image;
     }
 
 
