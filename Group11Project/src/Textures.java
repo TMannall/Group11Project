133fe19bc591123072dbd750dbce29fac0342e17
Jack <jackkeates@gmail.com>
2016-01-26 12:38:31 +0000

    added aduio files and loaded extra textures
    see title

--- Group11Project/src/Textures.java
+++ Group11Project/src/Textures.java
@@ -7,14 +7,30 @@ import java.nio.file.Paths;
 
 public class Textures {
 
-    public Sprite mainMenu;
-    public Sprite captainTestPortrait;
+    // splash screens
+    public Sprite mainMenu = LoadTexture("textures/main_menu.png");
+    public Sprite ocean = LoadTexture("textures/ocean.png");
 
-    public Textures() {
-        mainMenu = LoadTexture("textures/main_menu.png");
-        captainTestPortrait = LoadTexture("textures/captain_portrait_example.png");
-    }
+    // user interface (will be frames)
+    public Sprite userInterface = LoadTexture("textures/user_interface.png");
+    public Sprite sailor1 = LoadTexture("textures/sailor_1.png");
 
+    // ship models
+    public Sprite shipLv1 = LoadTexture("textures/ship_level_1.png");
+    public Sprite shipLv2 = LoadTexture("textures/ship_level_1.png");
+    public Sprite shipLv3 = LoadTexture("textures/ship_level_3.png");
+
+    // character portraits
+    public Sprite captainTestPortrait = LoadTexture("textures/captain_portrait_example.png");
+
+
+    /**
+     * takes the location of the texture and returns the image as a sprite ready
+     * to be used in-game
+     *
+     * @param fileName
+     * @return
+     */
     public Sprite LoadTexture(String fileName) {
         Texture texture = new Texture( );
 
@@ -33,6 +49,7 @@ public class Textures {
         return sprite;
     }
 
+    
     public Sprite LoadFrameTexture(String fileName) {
         Texture texture = new Texture( );
 
