8ca4c7b9fc24924f92b8e6ddeea4626f443b5193
Jack <jackkeates@gmail.com>
2016-02-08 16:59:00 +0000

    texture and sprite loading adjusted
    textures are loaded separately to sprites now, rather than this being
    done in the same method, this should allow to multiple, differently
    named, sprites to be created from one single texture

--- Group11Project/src/Menu.java
+++ Group11Project/src/Menu.java
@@ -102,24 +102,6 @@ public class Menu extends FSMState{
         textures.mainMenu.setPosition(driver.getWinWidth() / 2, driver.getWinHeight() / 2);
         window.draw(textures.mainMenu);
 
-        // jack: testing animations
-//        textures.ocean.setPosition(driver.getWinWidth() / 2, driver.getWinHeight() / 2);
-//        window.draw(textures.ocean);
-//
-//        textures.shipLv3.setPosition(driver.getWinWidth() / 2, driver.getWinHeight() / 2);
-//        textures.shipLv3.setScale(1.2f, 1.2f);
-//        window.draw(textures.shipLv3);
-//
-//        textures.britishMarineFire.setPosition(driver.getWinWidth() / 2 + 350, driver.getWinHeight() / 2 - 75);
-//        window.draw(textures.britishMarineFire);
-//        textures.spanishMarineFire.setPosition(driver.getWinWidth() / 2 + 400, driver.getWinHeight() / 2 - 80);
-//        window.draw(textures.spanishMarineFire);
-//        textures.frenchMarineFire.setPosition(driver.getWinWidth() / 2 + 450, driver.getWinHeight() / 2 - 70);
-//        window.draw(textures.frenchMarineFire);
-//        textures.neutralMarineFire.setPosition(driver.getWinWidth() / 2 + 500, driver.getWinHeight() / 2 - 85);
-//        window.draw(textures.neutralMarineFire);
-        // jack: testing animations end
-
         try {
             Thread.sleep(10); //1000); // this delays the animation frames if it's too high
         } catch (InterruptedException e) {
