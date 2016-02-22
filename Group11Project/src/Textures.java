19e34830b73842a5f68570ed918a3e658809b97d
Jack <jackkeates@gmail.com>
2016-02-22 15:06:06 +0000

    fixed things and updated minor menu things, no tom i wont add a face emoticon thing

--- Group11Project/src/Textures.java
+++ Group11Project/src/Textures.java
@@ -10,14 +10,14 @@ public class Textures {
     // Splash screens
     private Texture mainMenu_ = loadTexture("textures/splash_main_menu.png");
     public Sprite mainMenu = createSprite(mainMenu_, 0, 0, 1920, 1080);
+    private Texture gameover_ = loadTexture("textures/splash_game_over.png");
+    public Sprite gameover = createSprite(gameover_, 0, 0, 1920, 1080);
     private Texture ocean_ = loadTexture("textures/ocean.png");
     public Sprite ocean = createSprite(ocean_, 0, 0, 1280, 720);
     private Texture mapConnections_ = loadTexture("textures/MapConnections.png");
     public Sprite mapConnections = createSprite(mapConnections_, 0, 0, 1280, 720);
     private Texture splashTitle_ = loadTexture("textures/main_menu_title.png");
     public Sprite splashTitle = createSprite(splashTitle_, 0, 0, 720, 171);
-    public Texture gameover_ = loadTexture("textures/splash_game_over.png");
-    public Sprite gameover = createSprite(gameover_, 0, 0, 1280, 720);
 
     public Texture instructions = loadTexture("textures/Intructions.png");
     public Sprite instMap = createSprite(instructions, 0, 0, 1280, 720);
@@ -58,8 +58,8 @@ public class Textures {
     public Sprite buttonSmall = createSprite(userInterface, 300, 21, 125, 60); // hover: 300, 100, 125, 60   push: 300, 179, 125, 60
     public Sprite shipIcon = createSprite(userInterface, 549, 11, 254, 92);
     public Sprite waypoint = createSprite(userInterface, 466, 24, 56, 56); // visited: 466, 103, 56, 56
-    // events
-    public Texture messageScroll_ = loadTexture("textures/Assembled_Scrol3.png");
+        // events
+    public Texture messageScroll_ = loadTexture("textures/Assembled_Scroll3.png");
     public Sprite messageScroll = createSprite(messageScroll_, 0, 0, 782, 713);
 
     public Sprite leftArrow = createSprite(userInterface, 945, 579, 70, 51);
@@ -99,11 +99,11 @@ public class Textures {
     public Sprite chainshot = createSprite(userInterface, 1004, 532, 14, 23);
 
     // Ship models
-    private Texture shipLv1 = loadTexture("textures/ship_level_1.png");
-    private Texture shipLv2 = loadTexture("textures/ship_level_2.png");
-    private Texture shipLv3 = loadTexture("textures/ship_level_3.png");
-    private Texture shipLv3Dead = loadTexture("textures/ship_level_3_destroyed.png");
-    private Texture shipHighlight = loadTexture("textures/ship_highlights.png");
+    public Texture shipLv1 = loadTexture("textures/ship_level_1.png");
+    public Texture shipLv2 = loadTexture("textures/ship_level_2.png");
+    public Texture shipLv3 = loadTexture("textures/ship_level_3.png");
+    public Texture shipLv3Dead = loadTexture("textures/ship_level_3_destroyed.png");
+    public Texture shipHighlight = loadTexture("textures/ship_highlights.png");
         // player ship
     public Sprite shipBridge = createSprite(shipLv3, 0, 0, 525, 365);
     public Sprite shipGunDeck = createSprite(shipLv3, 525, 0, 343, 125);
