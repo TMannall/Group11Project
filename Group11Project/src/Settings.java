17fdb7a493fd5720d9596ca18b5849f49e74b067
Will Lau <wlau11@hotmail.co.uk>
2016-02-23 08:44:07 +0000

    Implemented Sound setting

--- Group11Project/src/Settings.java
+++ Group11Project/src/Settings.java
@@ -7,7 +7,9 @@ import org.jsfml.window.event.Event;
 import org.jsfml.window.event.KeyEvent;
 import org.jsfml.graphics.Sprite;
 import org.jsfml.graphics.FloatRect;
+import org.jsfml.graphics.IntRect;
 import org.jsfml.window.event.MouseEvent;
+import org.jsfml.window.Mouse;
 
 import java.io.File;
 import java.io.IOException;
@@ -24,11 +26,17 @@ public class Settings extends FSMState{
     private Textures textures;
     private SoundFX sound;
 
-    private static int numberOfButtons = 4;
+    private static int numberOfButtons = 3;
+	private static int numberOfArrows = 2;
     private static int buttonIndex = 0;
     Text[] text = new Text[numberOfButtons];
     Text title;
 	FloatRect[] rect = new FloatRect[numberOfButtons];
+	FloatRect[] leftArrowRect = new FloatRect[numberOfArrows];
+	FloatRect[] rightArrowRect = new FloatRect[numberOfArrows];
+	IntRect[] recti = new IntRect[1];
+	float musicVol = 100;
+	float soundVol = 100;
 
     private static String JavaVersion = Runtime.class.getPackage().getImplementationVersion();
     private static String JdkFontPath = "textures/";
@@ -88,15 +96,9 @@ public class Settings extends FSMState{
 
         text[2].setFont(sansRegular);
         text[2].setColor(BROWN);
-        text[2].setString("Res: " + GameDriver.getWinWidth() + " x " + GameDriver.getWinHeight());
+        text[2].setString("Main Menu");
         text[2].setPosition(driver.getWinWidth() / 2, 400);
 		text[2].setOrigin(text[2].getLocalBounds().width / 2, text[2].getLocalBounds().height / 2);
-
-        text[3].setFont(sansRegular);
-        text[3].setColor(BROWN);
-        text[3].setString("Main Menu");
-        text[3].setPosition(driver.getWinWidth() / 2, 470);
-		text[3].setOrigin(text[3].getLocalBounds().width / 2, text[3].getLocalBounds().height / 2);
     }
 
     @Override
@@ -123,6 +125,14 @@ public class Settings extends FSMState{
 		Sprite[] textButton = new Sprite[numberOfButtons];
 		Sprite[] hoverButton = new Sprite[numberOfButtons];
 		Sprite[] pushButton = new Sprite[numberOfButtons];
+		Sprite[] leftArrow = new Sprite[numberOfArrows];
+		Sprite[] rightArrow = new Sprite[numberOfArrows];
+		
+		
+		for(int i = 0; i < numberOfArrows; i++){
+			leftArrow[i] = textures.createSprite(textures.userInterface, 945, 579, 70, 51);
+			rightArrow[i] = textures.createSprite(textures.userInterface, 1016, 579, 70, 51);
+		}
 		
 		for(int i = 0; i < numberOfButtons; i++){
 			textButton[i] = textures.createSprite(textures.userInterface, 23, 21, 250, 60);
@@ -145,29 +155,55 @@ public class Settings extends FSMState{
             window.draw(textButton[i]);
             window.draw(text[i]);
         }
+		
+		leftArrow[0].setPosition(text[0].getPosition().x - 170, text[0].getPosition().y + 8);
+		rightArrow[0].setPosition(text[0].getPosition().x + 170, text[0].getPosition().y + 8);
+		leftArrow[1].setPosition(text[1].getPosition().x - 170, text[1].getPosition().y + 8);
+		rightArrow[1].setPosition(text[1].getPosition().x + 170, text[1].getPosition().y + 8);
 			
 		textButton[0].setPosition(text[0].getPosition().x, text[0].getPosition().y + 8);
 		textButton[1].setPosition(text[1].getPosition().x, text[1].getPosition().y + 8);
 		textButton[2].setPosition(text[2].getPosition().x, text[2].getPosition().y + 8);
-		textButton[3].setPosition(text[3].getPosition().x, text[3].getPosition().y + 8);
+		//textButton[3].setPosition(text[3].getPosition().x, text[3].getPosition().y + 8);
 			
 		hoverButton[0].setPosition(text[0].getPosition().x, text[0].getPosition().y + 8);
 		hoverButton[1].setPosition(text[1].getPosition().x, text[1].getPosition().y + 8);
 		hoverButton[2].setPosition(text[2].getPosition().x, text[2].getPosition().y + 8);
-		hoverButton[3].setPosition(text[3].getPosition().x, text[3].getPosition().y + 8);
+		//hoverButton[3].setPosition(text[3].getPosition().x, text[3].getPosition().y + 8);
 			
 		pushButton[0].setPosition(text[0].getPosition().x, text[0].getPosition().y + 8);
 		pushButton[1].setPosition(text[1].getPosition().x, text[1].getPosition().y + 8);
 		pushButton[2].setPosition(text[2].getPosition().x, text[2].getPosition().y + 8);
-		pushButton[3].setPosition(text[3].getPosition().x, text[3].getPosition().y + 8);
+		//pushButton[3].setPosition(text[3].getPosition().x, text[3].getPosition().y + 8);
 
         for (int i = 0; i < numberOfButtons; i++) {
 			//window.draw(textButton[i]);
             window.draw(text[i]);
         }
 		
-		window.draw(textButton[3]);
-		window.draw(text[3]);
+		for(int i = 0; i < numberOfArrows; i++){
+			window.draw(leftArrow[i]);
+			window.draw(rightArrow[i]);
+		}
+		
+        rect[0] = new FloatRect(textButton[2].getGlobalBounds().left, textButton[2].getGlobalBounds().top,
+								textButton[2].getGlobalBounds().width, textButton[2].getGlobalBounds().height);
+        recti[0] = new IntRect(rect[0]);
+		
+        if((recti[0].contains(Mouse.getPosition(window)) && isMouseOver())){
+            textButton[2].setTextureRect(new IntRect(23, 100, 250, 60));
+            window.draw(textButton[2]);
+            window.draw(text[2]);
+        }
+        else if(!isMouseOver()){
+            textButton[2].setTextureRect(new IntRect(23, 21, 250, 60));
+            window.draw(textButton[2]);
+            window.draw(text[2]);
+        }
+  
+		
+		/*window.draw(textButton[3]);
+		window.draw(text[3]);*/
 
         for (Event event : window.pollEvents()) {
             switch (event.type) {
@@ -199,20 +235,57 @@ public class Settings extends FSMState{
                             case 2: //Settings 3
 
                                 break;
-                            case 3: //Main menu
+                            /*case 3: //Main menu
                                 stateMachine.setState(stateMachine.getStates().get(0));
-                                break;
+                                break;*/
                         }
                     }
 					break;
 				case MOUSE_BUTTON_PRESSED:
 					MouseEvent mouseClicked = event.asMouseButtonEvent();
 					if(mouseClicked.type == Event.Type.MOUSE_BUTTON_PRESSED){
-						//System.out.println("Pos Clicked: " + mouseClicked.position);
+						
 						for(int i = 0; i < numberOfButtons; i++){
 							rect[i] = new FloatRect(textButton[i].getGlobalBounds().left, textButton[i].getGlobalBounds().top, 
 										textButton[i].getGlobalBounds().width, textButton[i].getGlobalBounds().height);
 						}
+						
+						for(int i = 0; i < numberOfArrows; i++){
+							leftArrowRect[i] = new FloatRect(leftArrow[i].getGlobalBounds().left, leftArrow[i].getGlobalBounds().top, 
+															leftArrow[i].getGlobalBounds().width, leftArrow[i].getGlobalBounds().height);
+							rightArrowRect[i] = new FloatRect(rightArrow[i].getGlobalBounds().left, rightArrow[i].getGlobalBounds().top, 
+															rightArrow[i].getGlobalBounds().width, rightArrow[i].getGlobalBounds().height);
+						}
+						
+						//Music down
+						if(leftArrowRect[0].contains(mouseClicked.position.x, mouseClicked.position.y)){
+							musicVol -= 10;
+							sound.setMusicVolume(musicVol);
+							text[0].setString("Music : " + sound.getMusicVolume() + "%");
+							window.draw(text[0]);
+						}
+						//Music up
+						if(rightArrowRect[0].contains(mouseClicked.position.x, mouseClicked.position.y)){
+							musicVol += 10;
+							sound.setMusicVolume(musicVol);
+							text[0].setString("Music : " + sound.getMusicVolume() + "%");
+							window.draw(text[0]);
+						}
+						//Sound down
+						if(leftArrowRect[1].contains(mouseClicked.position.x, mouseClicked.position.y)){
+							soundVol -= 10;
+							sound.setSoundVolume(soundVol);
+							text[1].setString("Sound : " + sound.getSoundVolume() + "%");
+							window.draw(text[1]);
+						}
+						//Sound up
+						if(rightArrowRect[1].contains(mouseClicked.position.x, mouseClicked.position.y)){
+							soundVol += 10;
+							sound.setSoundVolume(soundVol);
+							text[1].setString("Sound : " + sound.getSoundVolume() + "%");
+							window.draw(text[1]);
+						}
+						
 						//Music Vol
 						if(rect[0].contains(mouseClicked.position.x, mouseClicked.position.y)){
 							window.draw(pushButton[0]);
@@ -225,15 +298,10 @@ public class Settings extends FSMState{
 							window.draw(pushButton[1]);
 							window.draw(text[1]);
 						}
-						//Settings 3
+						//Main Menu
 						if(rect[2].contains(mouseClicked.position.x, mouseClicked.position.y)){
 							window.draw(pushButton[2]);
 							window.draw(text[2]);
-						}
-                        //Main Menu
-						if(rect[3].contains(mouseClicked.position.x, mouseClicked.position.y)){
-							window.draw(pushButton[3]);
-							window.draw(text[3]);
 							stateMachine.setState(stateMachine.getStates().get(0));
 						}
 					}
@@ -242,6 +310,8 @@ public class Settings extends FSMState{
 			}
 		window.display();
     }
+	
+	//public void 
 
     public void moveUp() {
         if (buttonIndex - 1 >= 0) {
@@ -261,5 +331,12 @@ public class Settings extends FSMState{
 
     int getButtonIndex(){
         return buttonIndex;
+    }
+	
+    public boolean isMouseOver(){
+        if(recti[0].contains(Mouse.getPosition(window))){
+            return true;
+        }
+        return false;
     }
 }
